ce3686ad30a80c39f9591f04dcbd019eadc4d1e9
Ivan Bacigaluppo <ivanbac_96@hotmail.com>
2018-09-12 22:36:26 -0300

    Reordenando el controller

--- index.js
+++ index.js
@@ -1,23 +1,11 @@
 var express = require('express');
+var routes = require('./config/routes')
 
 var app = express();
 
 var port = process.env.PORT || 8080;
 
-var bookRouter = express.Router();
-
-bookRouter.route('/books')
-    .get(function (req, res) {
-        var responseJson = {hello: "This is my api"};
-
-        res.json(responseJson)
-    });
-
-app.use('/api', bookRouter);
-
-app.get('/', function (req, res) {
-    res.send('Hello world! Travis lcdtm. Funciona o te mato 1er aviso.');
-})
+routes(app);
 
 app.listen(port, function () {
     console.log('Runing on Port:' + port);
