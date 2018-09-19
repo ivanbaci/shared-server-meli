dc221c99989d80eeea528d68417100f4e7ebc6e1
Ivan Bacigaluppo <ivanbac_96@hotmail.com>
2018-09-19 00:24:34 -0300

    Deployando a Heroku

--- test/test.js
+++ test/test.js
@@ -6,13 +6,3 @@ describe('Array', function() {
         });
     });
 });
-
-
-var assert = require('assert');
-describe('Array', function() {
-    describe('#indexOf()', function() {
-        it('should return -1 when the value is not present', function() {
-            assert.equal([1,2,3].indexOf(4), -1);
-        });
-    });
-});
