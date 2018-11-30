13f451b66c496cdeb63a94af16d7383981326eef
Ivan Bacigaluppo <ivanbac_96@hotmail.com>
2018-11-30 17:28:57 -0300

    Changes in database

--- api/models/db.js
+++ api/models/db.js
@@ -1,19 +1,24 @@
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize("meli", "postgres", "root", {
-	host: "localhost",
-	dialect: "postgres",
-	operatorsAliases: false,
+const sequelize = new Sequelize(
+	"d23n0j4eh8pqta",
+	"danuwcbygcvjwf",
+	"6e9b2ee0d2b622e86ee3f7a5cff56a844c844ac3a79315d540da9b2c24ac9c15",
+	{
+		host: "localhost",
+		dialect: "postgres",
+		operatorsAliases: false,
 
-	pool: {
-		max: 5,
-		min: 0,
-		acquire: 30000,
-		idle: 10000
-	},
-	logging: false,
-	timezone: "-03:00" //TODO: no funciona bien el timezone
-});
+		pool: {
+			max: 5,
+			min: 0,
+			acquire: 30000,
+			idle: 10000
+		},
+		logging: false,
+		timezone: "-03:00" //TODO: no funciona bien el timezone
+	}
+);
 
 sequelize
 	.authenticate()
