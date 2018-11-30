9af6923253e4ed2980090bf063ac73f0d3d1ff9d
Ivan Bacigaluppo <ivanbac_96@hotmail.com>
2018-11-30 15:56:46 -0300

    Test post server

--- test/server.js
+++ test/server.js
@@ -9,12 +9,12 @@ let should = chai.should();
 chai.use(chaiHttp);
 
 describe("Servers", () => {
-    beforeEach((done) => {
-        Server.remove({}, (err) => {
-            done();
-        })
-    }
-})
+	beforeEach(done => {
+		Server.remove({}, err => {
+			done();
+		});
+	});
+});
 
 describe("/GET server", () => {
 	it("it should get all the servers", done => {
@@ -24,6 +24,85 @@ describe("/GET server", () => {
 				res.should.have.status(200);
 				res.body.should.be.a("array");
 				res.body.length.should.be.eql(0);
+				done();
+			});
+	});
+});
+
+describe("/POST server", () => {
+	it("it should post a new server with all his fields corrects", done => {
+		let server = {
+			id: "123",
+			_rev: "prueba",
+			createdBy: "Ivan Bacigaluppo",
+			name: "Test server"
+		};
+
+		chai.request(app)
+			.post("/server")
+			.send(server)
+			.end((err, res) => {
+				res.should.have.status(201);
+				done();
+			});
+	});
+
+	it("it should not post a server withot a name", done => {
+		let server = {
+			id: "123",
+			_rev: "prueba",
+			createdBy: "Ivan Bacigaluppo"
+		};
+
+		chai.request(app)
+			.post("/server")
+			.send(server)
+			.end((err, res) => {
+				res.should.have.status(400);
+				res.body.should.be.a("object");
+				res.body.should.have.property("message");
+				res.body.message.should.be.a("array");
+				res.body.message[0].should.eql('"name" is required');
+				done();
+			});
+	});
+
+	it("it should not post a server withot created by", done => {
+		let server = {
+			id: "123",
+			_rev: "prueba",
+			name: "Server de prueba"
+		};
+
+		chai.request(app)
+			.post("/server")
+			.send(server)
+			.end((err, res) => {
+				res.should.have.status(400);
+				res.body.should.be.a("object");
+				res.body.should.have.property("message");
+				res.body.message.should.be.a("array");
+				res.body.message[0].should.eql('"createdBy" is required');
+				done();
+			});
+	});
+
+	it("it should not post a server withot a rev", done => {
+		let server = {
+			id: "123",
+			createdBy: "Ivan Bacigaluppo",
+			name: "Server de prueba"
+		};
+
+		chai.request(app)
+			.post("/server")
+			.send(server)
+			.end((err, res) => {
+				res.should.have.status(400);
+				res.body.should.be.a("object");
+				res.body.should.have.property("message");
+				res.body.message.should.be.a("array");
+				res.body.message[0].should.eql('"_rev" is required');
 				done();
 			});
 	});
